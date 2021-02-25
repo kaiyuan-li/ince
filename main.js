8b9d22efa88a0c7bb00101259e3e7b3fcc09b393
K Li <kyli.nju@gmail.com>
2021-02-25 08:59:58 -0500

    multiple evaluations work

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const { random } = require('lodash');
 let Sandbox = require('./lib/Sandbox')
 
-const poolSize = 1
+const poolSize = 2
 let mySandbox = new Sandbox({poolSize})
 
 mySandbox.initialize(err => {
   if (err) throw new Error(`unable to initialize the sandbox: ${err}`)
 
-  const code = 'print "Hello, world!"'
+  const code = 'print("Hello world")'
   const timeoutMs = 2 * 1000
 
   console.log('server is up!')
   mySandbox.run({code, timeoutMs}, (err, result) => {
     if (err) throw new Error(`unable to run the code in the sandbox: ${err}`)
 
-    console.log(result.stdout); // Hello, world!
+    console.log("====Result is=======\n", result.stdout); // Hello, world!
+  })
+
+  let anotherCode = "print('Bye world!')"
+  mySandbox.run({code: anotherCode, timeoutMs}, (err, result) => {
+    if (err) throw new Error(`unable to run the code in the sandbox: ${err}`)
+
+    console.log("====Result is=======\n", result.stdout); // Hello, world!
   })
 });
